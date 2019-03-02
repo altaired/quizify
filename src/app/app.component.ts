e755e6ebc58f14a8bcf4f570d523e88798d12695
Simon Persson <simon@akep.se>
2019-03-02 17:31:11 +0100

    Initial spotify playback sdk setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+/// <reference types="spotify-web-playback-sdk" />
+
 import { Component } from '@angular/core';
+import { I18nSelectPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'app';
+  private player: Spotify.SpotifyPlayer;
+
+  constructor() {
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const token = 'BQBx5Isi0AFEjss1FZ_Jww7D69zf5_esqP0UnkUFRdzztp4ISuNwFbUQkEwj5R84sl1_zW0o2HAWwG1jxl0jQ782aqb0sDPXOnwhS0WFPH-iRrzYONLxgYXqb0mdFRIDo51HVcMDcbSmflcSfmqGM0hHdVX9FBYKxgze5F7j';
+      this.player = new Spotify.Player({
+        name: 'Web Playback SDK Quick Start Player',
+        getOAuthToken: cb => { cb(token); }
+      });
+      this.connectPlayer();
+    };
+  }
+
+  private connectPlayer() {
+    this.player.connect().then(success => console.log('Player connected!'));
+    this.player.addListener('player_state_changed', this.updatePlaybackState);
+  }
+
+  private updatePlaybackState(state: Spotify.PlaybackState) {
+    console.log(state);
+  }
+
+  togglePlay() {
+    this.player.togglePlay().then(() => console.log('Toggle!'));
+  }
+
 }
