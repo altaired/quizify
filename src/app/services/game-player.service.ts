1ad55d4b04c35959f2fc947f4ab1f32c8aae42a5
Simon Persson <simon@akep.se>
2019-03-18 19:55:54 +0100

    Joining games partially works

--- src/app/services/game-player.service.ts
+++ src/app/services/game-player.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AuthService } from './auth.service';
+import { take, map } from 'rxjs/operators';
+import { Player } from '../models/state';
+import { unescapeIdentifier } from '@angular/compiler';
 
 /**
  * Handles the players state during the game
@@ -10,5 +15,16 @@ import { Injectable } from '@angular/core';
 })
 export class GamePlayerService {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private db: AngularFireDatabase
+  ) { }
+
+  join(gameCode: string, name: string) {
+    this.auth.user$.pipe(take(1), map(user => user.uid))
+      .subscribe(uid => this.db.list('games/' + gameCode + '/players').push(({
+        uid: uid,
+        displayName: name
+      })));
+  }
 }
