35a777cbea6fc941b51752ecbba7984fec41d61d
Simon Persson <simon@akep.se>
2019-03-20 18:38:16 +0100

    Changed paths and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './core/login/login.component';
 import { PlaybackComponent } from './playback/playback.component';
-import { GameboardComponent } from './player/gameboard/gameboard.component';
+import { PlayerDisplayComponent } from './player/player-display/player-display.component';
 import { WelcomeComponent } from './host/welcome/welcome.component';
 import { HostGuard } from './core/host.guard';
-import { DisplayComponent } from './display/display.component';
+import { HostDisplayComponent } from './host/host-display/host-display.component';
 import { GameCreationComponent } from './host/game-creation/game-creation.component';
 
 const routes: Routes = [
-  { path: 'display', component: DisplayComponent, canActivate: [HostGuard] },
+  { path: 'display', component: HostDisplayComponent, canActivate: [HostGuard] },
   { path: 'playback', component: PlaybackComponent, canActivate: [HostGuard] },
-  { path: 'game', component: GameboardComponent },
+  { path: 'game', component: PlayerDisplayComponent },
   { path: 'create-game', component: GameCreationComponent, canActivate: [HostGuard] },
   { path: 'welcome', component: WelcomeComponent, canActivate: [HostGuard] },
   { path: '', component: LoginComponent },
